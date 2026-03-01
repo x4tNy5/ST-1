6719ee870f4d56488723e757cefb28be63f26cdc
5yNt4x <5yNt4x@list.ru>
2026-03-01 22:15:34 +0300

    yup.

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,7 +6,7 @@ bool checkPrime(uint64_t value) {
     if (value < 2) return false;
     if (value == 2) return true;
     if (value % 2 == 0) return false;
-    for (uint64_t i = 3; i <= value; i += 2) {
+    for (uint64_t i = 3; i * i <= value; i += 2) {
         if (value % i == 0) return false;
     }
     return true;
