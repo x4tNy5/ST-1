1b1bbc67d6fef4cd85724640198514274806dd5e
5yNt4x <5yNt4x@list.ru>
2026-03-01 22:08:41 +0300

    Done55555.

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,8 +6,7 @@ bool checkPrime(uint64_t value) {
     if (value < 2) return false;
     if (value == 2) return true;
     if (value % 2 == 0) return false;
-    for (uint64_t i = 3; i <= value; i += 2) 
-    {
+    for (uint64_t i = 3; i <= value; i += 2) {
         if (value % i == 0) return false;
     }
     return true;
@@ -17,11 +16,9 @@ uint64_t nPrime(uint64_t n) {
     if (n == 0) return 0; 
     uint64_t count = 0;
     uint64_t candidate = 1;
-    while (count < n) 
-    {
+    while (count < n) {
         ++candidate;
-        if (checkPrime(candidate)) 
-        {
+        if (checkPrime(candidate)) {
             ++count;
         }
     }
@@ -30,8 +27,7 @@ uint64_t nPrime(uint64_t n) {
 
 uint64_t nextPrime(uint64_t value) {
     uint64_t candidate = value + 1;
-    while (!checkPrime(candidate)) 
-    {
+    while (!checkPrime(candidate)) {
         ++candidate;
     }
     return candidate;
@@ -39,10 +35,8 @@ uint64_t nextPrime(uint64_t value) {
 
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
-  for (uint64_t i = 2; i < hbound; i++) 
-  {
-    if (checkPrime(i)) 
-    {
+  for (uint64_t i = 2; i < hbound; i++) {
+    if (checkPrime(i)) {
       sum += i;
     }
   }
